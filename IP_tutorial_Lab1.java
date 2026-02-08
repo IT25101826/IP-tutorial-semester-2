37ef358a8d3ce360f6fe144da95c409a78de412a
IT25101826 <it25101826@my.sliit.lk>
2026-02-08 14:02:26 +0530

    Add files via upload

---
+++ IP_tutorial_Lab1.java
@@ -0,0 +1,17 @@
+public class IP_tutorial_Lab1 {
+public static void main(String[] arg) {
+
+	//a)Declare Variables 
+		int miles;
+		int yards;
+		double kilometers;
+	
+	//b)initialize variables
+		miles = 26;
+		yards = 385;
+		
+	//c)& d)expression to calculate kilometers and store the result
+		kilometers = (miles + yards / 1760.0) * 1.609;
+		
+    }
+}
